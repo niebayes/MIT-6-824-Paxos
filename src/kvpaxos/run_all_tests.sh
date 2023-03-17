fc54dbf1d509609280f6a38fc56cb424dc34e01a
niebayes <niebayes@gmail.com>
2023-03-17 16:09:53 +0800

    kvpaxos: seems the paxos throughput is low which makes the TestManyParitions timeouts

--- src/kvpaxos/run_all_tests.sh
+++ src/kvpaxos/run_all_tests.sh
@@ -1,4 +1,4 @@
 # python3 dstest --race --iter 100 --workers 10 --timeout 60 --output out.log TestBasic TestDone TestPartition TestUnreliable TestHole TestManyPartition
-# python3 dstest --race --iter 100 --workers 10 --timeout 60 --output out.log TestDone TestManyPartition
+python3 dstest --race --iter 100 --workers 10 --timeout 60 --output out.log TestBasic TestPartition TestUnreliable TestHole
 # python3 dstest --race --iter 100 --workers 10 --timeout 60 --output out.log TestDone
-python3 dstest --race --iter 100 --workers 10 --timeout 60 --output out.log TestManyPartition
+# python3 dstest --race --iter 100 --workers 10 --timeout 60 --output out.log TestManyPartition
